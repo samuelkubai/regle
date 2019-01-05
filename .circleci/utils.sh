a10d0d2f8f67c087e63eb2d4debbd53a2228a3ef
Samuel Kubai Kamau <samuel.kubai@andela.com>
2019-01-05 08:23:02 +0300

    bg(ci-config): update the utlis file and remove the extra environment variables.

--- .circleci/utils.sh
+++ .circleci/utils.sh
@@ -37,30 +37,30 @@ is_success() {
 require () {
     if [ -z ${2+x} ]; then error "Required variable ${1} has not been set"; fi
 }
-
-SERVICE_KEY_PATH=$HOME/service-account-key.json
-
-# assert required variables
-require PRODUCTION_COMPUTE_ZONE $PRODUCTION_COMPUTE_ZONE
-require PRODUCTION_CLUSTER_NAME $PRODUCTION_CLUSTER_NAME
-require PRODUCTION_STATIC_IP $PRODUCTION_STATIC_IP
-
-require STAGING_COMPUTE_ZONE $STAGING_COMPUTE_ZONE
-require STAGING_CLUSTER_NAME $STAGING_CLUSTER_NAME
-require STAGING_STATIC_IP $STAGING_STATIC_IP
-
-if [ "$CIRCLE_BRANCH" == 'master' ]; then
-    IMAGE_TAG=production-$(git rev-parse --short HEAD)
-    export ENVIRONMENT=production
-    export COMPUTE_ZONE=$PRODUCTION_COMPUTE_ZONE
-    export CLUSTER_NAME=$PRODUCTION_CLUSTER_NAME
-    export STATIC_IP=$PRODUCTION_STATIC_IP
-else
-    IMAGE_TAG=staging-$(git rev-parse --short HEAD)
-    export ENVIRONMENT=staging
-    export COMPUTE_ZONE=$STAGING_COMPUTE_ZONE
-    export CLUSTER_NAME=$STAGING_CLUSTER_NAME
-    export STATIC_IP=$STAGING_STATIC_IP
-fi
+#
+#SERVICE_KEY_PATH=$HOME/service-account-key.json
+#
+## assert required variables
+#require PRODUCTION_COMPUTE_ZONE $PRODUCTION_COMPUTE_ZONE
+#require PRODUCTION_CLUSTER_NAME $PRODUCTION_CLUSTER_NAME
+#require PRODUCTION_STATIC_IP $PRODUCTION_STATIC_IP
+#
+#require STAGING_COMPUTE_ZONE $STAGING_COMPUTE_ZONE
+#require STAGING_CLUSTER_NAME $STAGING_CLUSTER_NAME
+#require STAGING_STATIC_IP $STAGING_STATIC_IP
+#
+#if [ "$CIRCLE_BRANCH" == 'master' ]; then
+#    IMAGE_TAG=production-$(git rev-parse --short HEAD)
+#    export ENVIRONMENT=production
+#    export COMPUTE_ZONE=$PRODUCTION_COMPUTE_ZONE
+#    export CLUSTER_NAME=$PRODUCTION_CLUSTER_NAME
+#    export STATIC_IP=$PRODUCTION_STATIC_IP
+#else
+#    IMAGE_TAG=staging-$(git rev-parse --short HEAD)
+#    export ENVIRONMENT=staging
+#    export COMPUTE_ZONE=$STAGING_COMPUTE_ZONE
+#    export CLUSTER_NAME=$STAGING_CLUSTER_NAME
+#    export STATIC_IP=$STAGING_STATIC_IP
+#fi
 
 export NAMESPACE=$ENVIRONMENT
