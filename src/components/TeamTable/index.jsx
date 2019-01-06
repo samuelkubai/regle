78ae77d40bf44d0e11f54dd58a3c424d27a53b0c
Samuel Kubai Kamau <samuel.kubai@andela.com>
2019-01-06 23:28:57 +0300

    feat(team-table): hook up the team stats
    - hook up the team stats to the stats from the API
    - provide defaults for the team stats

--- src/components/TeamTable/index.jsx
+++ src/components/TeamTable/index.jsx
@@ -11,7 +11,15 @@ import Cookie from "cookies-js";
 
 export default class TeamTable extends Component {
   state = {
-    members: []
+    members: [],
+    team_stats: {
+      ready_fellows: 0,
+      fellows_on_track: 0,
+      team_total_velocity: 0
+    },
+    meta: {
+      count: 0
+    }
   };
 
   async componentDidMount() {
@@ -31,6 +39,7 @@ export default class TeamTable extends Component {
       return {
         ...state,
         members: users.data.users,
+        team_stats: users.data.team_stats,
         meta: users.meta
       }
     });
@@ -47,7 +56,7 @@ export default class TeamTable extends Component {
   }
 
   render() {
-    const { members } = this.state;
+    const { members, meta, team_stats } = this.state;
 
     return (
       <div className="team-table">
@@ -55,23 +64,27 @@ export default class TeamTable extends Component {
           <div className="team-table__stat">
             <div className="team-table__stat-title">Fellows ready</div>
             <div className="team-table__stat-container">
-              <span className="team-table__stat-value">9</span>
-              <span className="team-table__stat-against">/16</span>
+              <span className="team-table__stat-value">{ team_stats.ready_fellows }</span>
+              <span className="team-table__stat-against">/{ meta.count }</span>
             </div>
           </div>
 
           <div className="team-table__stat">
             <div className="team-table__stat-title">Fellows on-track</div>
             <div className="team-table__stat-container">
-              <span className="team-table__stat-value">11</span>
-              <span className="team-table__stat-against">/16</span>
+              <span className="team-table__stat-value">{ team_stats.fellows_on_track }</span>
+              <span className="team-table__stat-against">/{ meta.count }</span>
             </div>
           </div>
 
           <div className="team-table__stat">
             <div className="team-table__stat-title">Learning velocity</div>
             <div className="team-table__stat-container">
-              <span className="team-table__stat-value">910</span>
+              <span className="team-table__stat-value">{
+                meta.count === 0
+                  ? 0
+                  : (team_stats.team_total_velocity / meta.count).toFixed(0).toLocaleString()
+              }</span>
               <span className="team-table__stat-against">/wk</span>
             </div>
           </div>
